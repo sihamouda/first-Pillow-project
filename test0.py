6c8da28782243ec3cd81c81b2b95acfa08537072
sihamouda <anis.c.hamouda@gmail.com>
2022-02-14 16:54:55 +0100

    create a function that create a directory if it doesn't exist and used it in resizeALL

--- test0.py
+++ test0.py
@@ -1,10 +1,17 @@
 from PIL import Image
 import os 
 
+def CreateDir(path):
+    if not os.path.exists(path):
+        os.makedirs(path)
+        print("A new directory was created!")
+
 def resizeALL(pathSource,pathDestination):
     files=os.listdir(pathSource)
+    CreateDir(pathDestination)
     for file in files:
         if file.endswith(".jpg"):
             Image.open(file).resize((1620,2430)).save(pathDestination+"\\"+ os.path.splitext(file)[0]+"_resized.jpg")
 
-resizeALL(".","C:\\Users\\aniss\\Desktop\\PillowStuff\\NewFolder")
+
+resizeALL(".","C:\\Users\\aniss\\Desktop\\PillowStuff\\NewFolder2")
