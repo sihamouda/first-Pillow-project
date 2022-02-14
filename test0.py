22594ea5aca00e15b4a5e1823a64c40839b13c26
sihamouda <anis.c.hamouda@gmail.com>
2022-02-14 15:56:05 +0100

    create a function that resize all images in a specific directory and save it in the same one

--- test0.py
+++ test0.py
@@ -1,11 +1,10 @@
-
 from PIL import Image
 import os 
 
-files=os.listdir('.')
+def resizeALL(path):
+    files=os.listdir(path)
+    for file in files:
+        if file.endswith(".jpg"):
+            Image.open(file).resize((1620,2430)).save(os.path.splitext(file)[0]+"_resized.jpg")
 
-for file in files:
-    if file.endswith(".jpg"):
-        Image.open(file).resize((1620,2430)).save(os.path.splitext(file)[0]+"_resized.jpg")
-
-
+resizeALL(".")
