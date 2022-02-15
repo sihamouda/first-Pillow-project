b4d1181bb665ae8e40c290ae13abae24ead5d5de
sihamouda <anis.c.hamouda@gmail.com>
2022-02-15 10:03:33 +0100

    add to the function watermark: the logo will be resized so it can fit the image

--- test0.py
+++ test0.py
@@ -19,14 +19,17 @@ def resizeALL(pathSource,pathDestination):
 
 #watermark a specific image with a logo given an saved to a chosen path with the name new.jpg
 def watermark(pathImage,pathLogo,pathSave):
-    logo=Image.open(pathLogo)
     image=Image.open(pathImage)
+    #resize the logo as the givin image so we can watermark even if the image is smaller than the watermark as they have the same ratio
+    logo=Image.open(pathLogo).resize(image.width,image.height) 
     position = ((image.width - logo.width), (image.height - logo.height))
     image.paste(logo, position,logo)
     image.save(pathSave+'/new.jpg')
 
 #testing
 pathLogo='C:\\Users\\aniss\\Desktop\\PillowStuff\\Watermark\\wm.png'
-pathImage='C:\\Users\\aniss\\Desktop\\PillowStuff\\TLP_0653-Edit.jpg'
+pathImage='C:\\Users\\aniss\\Desktop\\PillowStuff\\TLP_0653-Edit_resized.jpg'
 pathSave='C:\\Users\\aniss\\Desktop\\PillowStuff'
+
+resizeALL(pathSave,pathSave)
 watermark(pathImage,pathLogo,pathSave)
